bb995d24292f5fc7139dfabff89b5f19e44a9c62
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-02-08 16:44:39 -0500

    accept multiple word entry

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,24 +1,26 @@
 # ask user to input a message
 puts "Please enter your word:"
 chosen_word = gets.chomp # gets.chomp returns a string. That's what we want here.
+chosen_word_array = chosen_word.split(" ")
+puts "#{chosen_word_array}"
 
 # define dictionary
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(chosen_word, dictionary)
+def substrings(chosen_word_array, dictionary)
   # define empty results hash
   results = {}
   
   # loop through each word in the dictionary to see if it matches the chosen word.
   dictionary.each do |word|
-    if word == chosen_word # if the word matches the chosen word, add it & number of times is appears to hash
-      num_times = dictionary.count(chosen_word)
-      results[word] = num_times
+    chosen_word_array.each do |chosen_word_var|
+      if word == chosen_word_var # if the word matches the chosen word, add it & number of times is appears to hash
+        num_times = dictionary.count(chosen_word_var)
+        results[word] = num_times
+      end
     end
   end
-
-
   puts results
 end
 
-substrings(chosen_word, dictionary)
+substrings(chosen_word_array, dictionary)
