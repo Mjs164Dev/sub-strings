1b38c3b366ef77a8a4d84155fd1bd4eddb05060f
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-02-08 14:43:23 -0500

    "Add count logic and push word and count to hash if count > 0"

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,9 +1,24 @@
 # ask user to input a message
-puts "Please enter your message:"
-message = gets.chomp # gets.chomp returns a string. That's what we want here.
+puts "Please enter your word:"
+chosen_word = gets.chomp # gets.chomp returns a string. That's what we want here.
 
+# define dictionary
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(message, dictionary)
+def substrings(chosen_word, dictionary)
+  # define empty results hash
+  results = {}
+  
+  # loop through each word in the dictionary to see if it matches the chosen word.
+  dictionary.each do |word|
+    if word == chosen_word # if the word matches the chosen word, add it & number of times is appears to hash
+      num_times = dictionary.count(chosen_word)
+      results[word] = num_times
+    end
+  end
 
+
+  puts results
 end
+
+substrings(chosen_word, dictionary)
