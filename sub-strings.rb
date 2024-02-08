7bc8e1ae575b30f60d590794f0d550aaebe28e09
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-02-08 14:22:55 -0500

    ask user for input & define dictionary.

---
+++ sub-strings.rb
@@ -0,0 +1,9 @@
+# ask user to input a message
+puts "Please enter your message:"
+message = gets.chomp # gets.chomp returns a string. That's what we want here.
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(message, dictionary)
+
+end
